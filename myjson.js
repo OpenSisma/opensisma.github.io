13c4941b1f69e7faa2996a78b3cd6a4251a9e8fc
opensisma <openaccess2020@libero.it>
2020-03-10 09:45:01 +0100

    Update myjson.js

--- myjson.js
+++ myjson.js
@@ -427,22 +427,20 @@ var myJSON = [
  //indice 
 
 
-
-
-var datiedu3 = {"labels": ['Somma lavori incompiuti', 'OO'],
-              "datasets": [{label: 'EEE',
-                            data: myJSON.map(itm=>itm['Value 1']+itm['Value 2']/itm.DANNO),
-                            data: myJSON.map(itm=>itm['Value 1']+itm['Value 2']/itm.DANNO),
-                            backgroundColor: 'rgb(255, 99, 132)',
-                            borderWidth: 1}] 
-               };
-function grafo2(dati, opzioni) {
-  var grafoline = document.getElementById('Chartline').getContext('2d');
-  new Chart(grafoline, {type: 'line',data: dati,options: opzioni});
-};
+//var datiedu3 = {"labels": ['Somma lavori incompiuti', 'OO'],
+             // "datasets": [{label: 'EEE',
+                      //      data: myJSON.map(itm=>itm['Value 1']+itm['Value 2']/itm.DANNO),
+                        //    data: myJSON.map(itm=>itm['Value 1']+itm['Value 2']/itm.DANNO),
+                          //  backgroundColor: 'rgb(255, 99, 132)',
+                            //borderWidth: 1}] 
+             //  };
+//function grafo2(dati, opzioni) {
+ // var grafoline = document.getElementById('Chartline').getContext('2d');
+  //new Chart(grafoline, {type: 'line',data: dati,options: opzioni});
+//};
 // display the data used in dataset[0]:
-console.log(datiedu3.datasets[0].data)
-grafo2(datiedu3)
+//console.log(datiedu3.datasets[0].data)
+//grafo2(datiedu3)
 
 
 
